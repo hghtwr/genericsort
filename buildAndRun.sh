198de09613f2ed6ad01353f6908ab77e07d7eddb
Giovannissimo <giovannissimo@magenta.de>
2022-07-24 17:40:08 +0200

    working jar building

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,3 +2,4 @@
 
 javac -d /home/development/de/akad/jav41/genericsorting/src/bin /home/development/de/akad/jav41/genericsorting/src/jav41/genericsorting/*.java
 java -cp /home/development/de/akad/jav41/genericsorting/src/bin/ jav41.genericsorting.Main
+jar cvfM program.jar -C /home/development/de/akad/jav41/genericsorting/src/bin/ .
