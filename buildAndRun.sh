7f40f662c454f348bd74c4f6c59c58529d4ef893
Giovannissimo <giovannissimo@magenta.de>
2022-08-26 12:26:31 +0200

    last commit

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,7 +2,8 @@
 
 javac -d /home/development/de/akad/jav41/genericsorting/src/bin /home/development/de/akad/jav41/genericsorting/src/jav41/genericsorting/*.java
 #java -cp /home/development/de/akad/jav41/genericsorting/src/bin/ -agentpath:/usr/share/visualvm/profiler/lib/deployed/jdk16/linux-amd64/libprofilerinterface.so=/usr/share/visualvm/profiler/lib,5140 jav41.genericsorting.Main
-java -cp /home/development/de/akad/jav41/genericsorting/src/bin/  jav41.genericsorting.Main -XX:+HeapDumpOnOutOfMemoryError XX:+UseCountedLoopSafepoint XX:HeapDumpPath=/tmp
+#java -cp /home/development/de/akad/jav41/genericsorting/src/bin/  jav41.genericsorting.Main -XX:+HeapDumpOnOutOfMemoryError XX:+UseCountedLoopSafepoint XX:HeapDumpPath=/tmp
 
 
-#jar cvfM program.jar -C /home/development/de/akad/jav41/genericsorting/src/bin/ .
+jar cvfM program.jar -C /home/development/de/akad/jav41/genericsorting/src/bin/ .
+java -jar program.jar
